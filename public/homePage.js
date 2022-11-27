3c3ea4bb8b60136999446eef2ddc89177c892e62
Margarita May Lee <ritochka.m_lee@yahoo.com>
2022-11-27 12:02:41 +0300

    Corrections V4.0

--- public/homePage.js
+++ public/homePage.js
@@ -2,14 +2,12 @@ const logout = new LogoutButton();
 logout.action = exit => ApiConnector.logout(response => {
     if (response.success) {
         location.reload();
-        return;
     }
 });
 
 ApiConnector.current(current => {
     if (current.success) {
         ProfileWidget.showProfile(current.data);
-        return;
     }
 });
 
@@ -19,38 +17,36 @@ function ratesUpdate() {
         if (response.success) {
            spreadsheet.clearTable();
            spreadsheet.fillTable(response.data);
-           return;
         }
     });
 }
-setInterval(ratesUpdate(), 1000);
+setInterval(function() {
+    ApiConnector.getStocks()
+  }, 1000);
 
 const addMoneyForm = new MoneyManager();
 addMoneyForm.addMoneyCallback = receipts => ApiConnector.addMoney(receipts, response => {
     if (response.success) {
-       addMoneyForm.addMoneyAction();
        ProfileWidget.showProfile(response.data);
-       return addMoneyForm.setMessage(true, 'Успешное пополнение счета на' + receipts.currency + receipts.amount);;
+       addMoneyForm.setMessage(true, 'Успешное пополнение счета на' + receipts.currency + receipts.amount);;
     }
-    return addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
+    addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
 });
 
 addMoneyForm.conversionMoneyCallback = exchange => ApiConnector.convertMoney(exchange, response => {
     if (response.success) {
-       addMoneyForm.conversionMoneyAction();
        ProfileWidget.showProfile(response.data);
-       return addMoneyForm.setMessage(true, 'Успешная конвертация суммы ' + exchange.fromCurrency + exchange.fromAmount);
+       addMoneyForm.setMessage(true, 'Успешная конвертация суммы ' + exchange.fromCurrency + exchange.fromAmount);
     }
-    return addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
+    addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
 });
 
 addMoneyForm.sendMoneyCallback = transfer => ApiConnector.transferMoney(transfer, response => {
     if (response.success) {
-       addMoneyForm.sendMoneyAction();
        ProfileWidget.showProfile(response.data);
-       return addMoneyForm.setMessage(true, 'Успешный перевод ' + transfer.currency + transfer.amount + ' получателю ' + transfer.to);
+       addMoneyForm.setMessage(true, 'Успешный перевод ' + transfer.currency + transfer.amount + ' получателю ' + transfer.to);
     }
-    return addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
+    addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
 });
 
 const favoriteSpreadsheet = new FavoritesWidget();
@@ -59,7 +55,6 @@ ApiConnector.getFavorites(response => {
        favoriteSpreadsheet.clearTable();
        favoriteSpreadsheet.fillTable(response.data);
        addMoneyForm.updateUsersList(response.data);
-       return;
     }
  });
 
@@ -68,9 +63,9 @@ ApiConnector.getFavorites(response => {
        favoriteSpreadsheet.clearTable();
        favoriteSpreadsheet.fillTable(response.data);
        addMoneyForm.updateUsersList(response.data);
-       return addMoneyForm.setMessage(true, 'Добавлен новый пользователь #' + addUser.id + ': ' + addUser.name);
+       favoriteSpreadsheet.setMessage(true, 'Добавлен новый пользователь #' + addUser.id + ': ' + addUser.name);
     }
-    return addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
+    favoriteSpreadsheet.setMessage(false, 'Ошибка: ' + response.error);
 });
 
 favoriteSpreadsheet.removeUserCallback = deletedUser => ApiConnector.removeUserFromFavorites(deletedUser, response => {
@@ -78,7 +73,7 @@ favoriteSpreadsheet.removeUserCallback = deletedUser => ApiConnector.removeUserF
        favoriteSpreadsheet.clearTable();
        favoriteSpreadsheet.fillTable(response.data);
        addMoneyForm.updateUsersList(response.data);
-       return addMoneyForm.setMessage(true, 'Пользователь ' + deletedUser + ' удален');
+       favoriteSpreadsheet.setMessage(true, 'Пользователь ' + deletedUser + ' удален');
     }
-    return addMoneyForm.setMessage(false, 'Ошибка: ' + response.error);
+    favoriteSpreadsheet.setMessage(false, 'Ошибка: ' + response.error);
 });
--- public/loginPage.js
+++ public/loginPage.js
@@ -2,15 +2,15 @@
 const userForm = new UserForm();
 userForm.loginFormCallback = data => ApiConnector.login(data, response => {
     if (!response.success) {
-        return userForm.setLoginErrorMessage(response.error);
+        userForm.setLoginErrorMessage(response.error);
     }
     location.reload();
-    return;
+
 });
 
 userForm.registerFormCallback = newData => ApiConnector.register(newData, response => {
     if (!response.success) {
-        return userForm.setRegisterErrorMessage(response.error);
+        userForm.setRegisterErrorMessage(response.error);
     }
     location.reload();
 });
